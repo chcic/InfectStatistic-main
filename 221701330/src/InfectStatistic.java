ad8fa2e153e2e795133344cf72748f20f02a3ab6
chcic <60641293+chcic@users.noreply.github.com>
2020-02-15 22:31:21 +0800

    sixth
    用java正则对日志信息进行匹配（未完成）

--- 221701330/src/InfectStatistic.java
+++ 221701330/src/InfectStatistic.java
@@ -24,8 +24,6 @@ class Province implements Comparable<Province>
         this.dead = dead;
     }
 
-    public Province() {}
-
     public int compareTo(Province a)
     {
         String province1 = this.province;
@@ -53,22 +51,55 @@ class InfectStatistic {
         return wrong;
     }
 
-    public static void Log(String Path)
+    public static void InformationProcessing(String Path,List<Province> proList) //日志信息处理
     {
         String pattern = ".*//.*";
-        boolean isMatch;
+        String provincepattern = ".*?\\s";
+        String pattern0 = "(\\d+)";
+        String pattern0_1 = "感染+";
+        String pattern0_2 = "疑似+";
+        String pattern1 = ".*新增.*";
+        String pattern2 = ".*流入.*";
+        String pattern3 = ".*死亡.*";
+        String pattern4 = ".*治愈.*";
+        String pattern5 = ".*确诊感染.*";
+        String pattern6 = ".*排除.*";
+        Pattern number = Pattern.compile(pattern0); // 创建 Pattern 对象
+        Pattern pro1 = Pattern.compile(provincepattern);
+        Pattern pro2 = Pattern.compile(provincepattern);
         try
         {
             File file = new File(Path);
-            if(!file .exists()) {
+            if(!file .exists())
+            {
                 System.out.println("文件不存在");
                 //System.exit(0);
             }
-            else{
+            else
+            {
                 BufferedReader br = new BufferedReader(new FileReader(file));
                 String str = null;
                 while ((str = br.readLine()) != null) {
                     if(Pattern.matches(pattern,str)) continue;
+                    // 现在创建 matcher 对象
+                    Matcher Pro1 = pro1.matcher(str);
+                    Matcher m = number.matcher(str);
+                    if (Pro1.find())
+                    {
+                        System.out.println("Found value: " + Pro1.group(0) );
+                    }
+                    if(Pattern.matches(pattern1,str))
+                    {
+                        if (m.find())
+                        {
+                            System.out.println("Found value: " + m.group(0) );
+                        }
+                    }
+                    else if(Pattern.matches(pattern2,str)) {}
+                    else if(Pattern.matches(pattern3,str)) {}
+                    else if(Pattern.matches(pattern4,str)) {}
+                    else if(Pattern.matches(pattern5,str)) {}
+                    else if(Pattern.matches(pattern6,str)) {}
                     System.out.println("读取的内容为：" + str);
                 }
             }
@@ -90,10 +121,10 @@ class InfectStatistic {
     }
     public static void main(String[] args)
     {
-        String pattern4 = "-date";
-        String pattern5 = "-type";
         String log = null,date = null,out = null;
         int listjudge = 0;
+        List<Province> proList = new ArrayList<>();
+        ArrayList<String> listFileName = new ArrayList<String>();
         for(int j = 0;j<args.length;j++) {
             if(args[j].equals("list")) listjudge++;
             else if(args[j].equals("-log"))
@@ -113,16 +144,14 @@ class InfectStatistic {
             System.out.println("命令错误");
             //System.exit(0);
         }
-        ArrayList<String> listFileName = new ArrayList<String>();
         getAllFileName(log,listFileName);
         for(String name:listFileName){
             if(name.contains(".txt")||name.contains(".properties")){
-                Log(name);
+                InformationProcessing(name,proList);
                 if(date==null) continue;
                 else if(name.equals(log+date+".log.txt")) break;
             }
         }
-        List<Province> proList = new ArrayList<>();
         Province pro1 = new Province("北京",3,4,5,6);
         Province pro2 = new Province("重庆",5,5,5,5);
         Province pro3 = new Province("湖北",1,2,3,4);
@@ -133,6 +162,5 @@ class InfectStatistic {
         for (Province province : proList) {
             province.printmessage();
         }
-        System.out.println(listjudge);
     }
 }
