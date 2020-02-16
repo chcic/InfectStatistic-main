b3e4044a64e3e289a8fa7b719f844b8206f3c827
chcic <60641293+chcic@users.noreply.github.com>
2020-02-16 21:31:50 +0800

    seventh
    完成对日志中新增患者情况处理，以及添加全国的总信息

--- 221701330/src/InfectStatistic.java
+++ 221701330/src/InfectStatistic.java
@@ -13,8 +13,8 @@ import  java.io.*;
 import  java.lang.*;
 class Province implements Comparable<Province>
 {
-    String province=null;
-    int ip = 0,sp = 0,cure = 0,dead = 0;
+    private String province = null;
+    private int ip = 0,sp = 0,cure = 0,dead = 0;
     public Province(String province,int ip,int sp,int cure,int dead)
     {
         this.province = province;
@@ -23,7 +23,51 @@ class Province implements Comparable<Province>
         this.cure = cure;
         this.dead = dead;
     }
+    public Province(String province)
+    {
+        this.province = province;
+    }
+    public void setprovince(String pro)
+    {
+        this.province = pro;
+    }
 
+    public String getprovince()
+    {
+        return this.province;
+    }
+    public void setip(int ip)
+    {
+        this.ip = ip;
+    }
+    public int getip()
+    {
+        return this.ip;
+    }
+    public void setsp(int sp)
+    {
+        this.sp = sp;
+    }
+    public int getsp()
+    {
+        return this.sp;
+    }
+    public void setcure(int cure)
+    {
+        this.cure = cure;
+    }
+    public int getcure()
+    {
+        return this.cure;
+    }
+    public void setdead(int dead)
+    {
+        this.dead = dead;
+    }
+    public int getdead()
+    {
+        return this.dead;
+    }
     public int compareTo(Province a)
     {
         String province1 = this.province;
@@ -35,6 +79,8 @@ class Province implements Comparable<Province>
         if(province2.indexOf("重") >= 0){
             province2 = province2.replaceAll("重", "冲");
         }
+        if(province1.equals("全国")) return -1; // 全国排第一
+        else if(province2.equals("全国")) return 1;
         return instance.compare(province1,province2);  //按拼音排序
     }
 
@@ -50,14 +96,27 @@ class InfectStatistic {
         boolean wrong = false;
         return wrong;
     }
-
-    public static void InformationProcessing(String Path,List<Province> proList) //日志信息处理
+    public static void addlist(String provence,List<Province> proList) // 判断列表中有没有该省份，无则加入
     {
+        boolean inlist = false;
+        for (Province province : proList) {
+            inlist = provence.equals(province.getprovince());
+            if(inlist) break ;
+        }
+        if(!inlist) // 若列表里没有该省份，加入列表
+        {
+            Province pro1 = new Province(provence);
+            proList.add(pro1);
+        }
+    }
+
+    public static void InformationProcessing(String Path,List<Province> proList,Province allpro) //日志信息处理
+    {
+        int ispro = 0,ip = 0,sp = 0,cure = 0,dead = 0;
         String pattern = ".*//.*";
-        String provincepattern = ".*?\\s";
         String pattern0 = "(\\d+)";
-        String pattern0_1 = "感染+";
-        String pattern0_2 = "疑似+";
+        String pattern0_1 = ".*感染患者.*";
+        String pattern0_2 = ".*疑似患者.*";
         String pattern1 = ".*新增.*";
         String pattern2 = ".*流入.*";
         String pattern3 = ".*死亡.*";
@@ -65,8 +124,6 @@ class InfectStatistic {
         String pattern5 = ".*确诊感染.*";
         String pattern6 = ".*排除.*";
         Pattern number = Pattern.compile(pattern0); // 创建 Pattern 对象
-        Pattern pro1 = Pattern.compile(provincepattern);
-        Pattern pro2 = Pattern.compile(provincepattern);
         try
         {
             File file = new File(Path);
@@ -80,27 +137,42 @@ class InfectStatistic {
                 BufferedReader br = new BufferedReader(new FileReader(file));
                 String str = null;
                 while ((str = br.readLine()) != null) {
-                    if(Pattern.matches(pattern,str)) continue;
-                    // 现在创建 matcher 对象
-                    Matcher Pro1 = pro1.matcher(str);
-                    Matcher m = number.matcher(str);
-                    if (Pro1.find())
-                    {
-                        System.out.println("Found value: " + Pro1.group(0) );
-                    }
-                    if(Pattern.matches(pattern1,str))
-                    {
-                        if (m.find())
+                    Matcher num = number.matcher(str); // 现在创建 matcher 对象
+                    String arrays[] = str.split(" ");
+                    if(Pattern.matches(pattern,str)) continue; // 跳过注释内容
+                    addlist(arrays[0],proList);
+                    for (Province province : proList) {
+                        if (arrays[0].equals(province.getprovince())&&num.find())
                         {
-                            System.out.println("Found value: " + m.group(0) );
+                            if(Pattern.matches(pattern1,str)) // 新增
+                            {
+                                if(Pattern.matches(pattern0_1,str)) // 新增感染患者
+                                {
+                                    ip = Integer.valueOf(num.group(0));
+                                    province.setip(province.getip()+ip);
+                                    allpro.setip(allpro.getip()+ip);
+                                }
+                                else if(Pattern.matches(pattern0_2,str)) //新增疑似患者
+                                {
+                                    sp = Integer.valueOf(num.group(0));
+                                    province.setsp(province.getsp()+sp);
+                                    allpro.setsp(allpro.getsp()+sp);
+                                }
+
+                            }
+                            else if(Pattern.matches(pattern2,str)) //从省一流入省二
+                            {
+                                addlist(arrays[3],proList); //判断省二有没有在列表里边
+
+                            }
+                            else if(Pattern.matches(pattern3,str)) {}
+                            else if(Pattern.matches(pattern4,str)) {}
+                            else if(Pattern.matches(pattern5,str)) {}
+                            else if(Pattern.matches(pattern6,str)) {}
                         }
                     }
-                    else if(Pattern.matches(pattern2,str)) {}
-                    else if(Pattern.matches(pattern3,str)) {}
-                    else if(Pattern.matches(pattern4,str)) {}
-                    else if(Pattern.matches(pattern5,str)) {}
-                    else if(Pattern.matches(pattern6,str)) {}
                     System.out.println("读取的内容为：" + str);
+                    ip = sp =cure = dead =0;
                 }
             }
         } catch (IOException  e) {
@@ -109,7 +181,6 @@ class InfectStatistic {
     }
     public static void getAllFileName(String path,ArrayList<String> listFileName){
         File file = new File(path);
-        File [] files = file.listFiles();
         String [] names = file.list();
         if(names != null){
             String [] completNames = new String[names.length];
@@ -125,6 +196,8 @@ class InfectStatistic {
         int listjudge = 0;
         List<Province> proList = new ArrayList<>();
         ArrayList<String> listFileName = new ArrayList<String>();
+        Province allpro = new Province("全国"); // 创建全国对象
+        proList.add(allpro);
         for(int j = 0;j<args.length;j++) {
             if(args[j].equals("list")) listjudge++;
             else if(args[j].equals("-log"))
@@ -147,17 +220,14 @@ class InfectStatistic {
         getAllFileName(log,listFileName);
         for(String name:listFileName){
             if(name.contains(".txt")||name.contains(".properties")){
-                InformationProcessing(name,proList);
+                InformationProcessing(name,proList,allpro);
                 if(date==null) continue;
                 else if(name.equals(log+date+".log.txt")) break;
             }
         }
-        Province pro1 = new Province("北京",3,4,5,6);
-        Province pro2 = new Province("重庆",5,5,5,5);
-        Province pro3 = new Province("湖北",1,2,3,4);
-        proList.add(pro1);
-        proList.add(pro2);
-        proList.add(pro3);
+        for (Province province : proList) {
+            province.printmessage();
+        }
         Collections.sort(proList);
         for (Province province : proList) {
             province.printmessage();
